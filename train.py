0b67fd2888fa2d8b7766d747f4355ef04c38c15e
haerimJeon <136302257+haerimJeon@users.noreply.github.com>
2023-06-13 02:03:25 +0000

    Update model training scheme

--- train.py
+++ train.py
@@ -36,17 +36,16 @@ if __name__ == "__main__":
     train_df = pd.read_csv(os.path.join(DATA_PATH, "house_rent_train.csv"))
 
     _X = train_df.drop(["rent", "area_locality", "posted_on"], axis=1)
-    y = np.log1p(train_df["rent"])
+    y = np.log1p(train_df["rent"])    
     
-    # TODO: X=_X, y=y로 전처리 파이프라인을 적용해 X에 저장
+    X = preprocess_pipeline.fit_transform(X=_X, y=y)
 
     # Data storage - 피처 데이터 저장
     if not os.path.exists(os.path.join(DATA_PATH, "storage")):
         os.makedirs(os.path.join(DATA_PATH, "storage"))
-    X.assign(rent=y).to_csv(
-        # TODO: DATA_PATH 밑에 storage 폴더 밑에 피처 데이터를 저장
-        
-        index=False,
+    X.assign(rent=y).to_csv(        
+        os.path.join(DATA_PATH, 'storage', 'feature_data.csv'),
+        index=False
     )
 
 
@@ -69,8 +68,8 @@ if __name__ == "__main__":
         with mlflow.start_run(run_name=f"Run {i}"):
             regr = GradientBoostingRegressor(**params)
             # 전처리 이후 모델 순서로 파이프라인 작성
-            pipeline = Pipeline(
-                # TODO: 전처리 파이프라인와 모델을 파이프라인으로 묶을 것
+            pipeline = Pipeline(               
+                [("preprocessor", preprocess_pipeline), ("regr", regr)]
             )
             pipeline.fit(_X, y)
 
@@ -86,7 +85,7 @@ if __name__ == "__main__":
             # 로깅 정보: 평가 메트릭
             mlflow.log_metrics(
                 {
-                    "RMSE_CV": #TODO: RMSE_CV 라는 이름으로 score_cv.mean()을 저장
+                    "RMSE_CV": score_cv.mean() 
                 }
             )
 
@@ -96,14 +95,12 @@ if __name__ == "__main__":
 
             # 모델 아티팩트 저장
             mlflow.sklearn.log_model(
-                # TODO: 최종 파이프라인을 저장
-                "model",
+                pipeline,
+                "model"
             )
 
             # log charts
-            mlflow.log_artifact(
-                # TODO: 아티팩트 경로 설정
-            )
+            mlflow.log_artifact(ARTIFACT_PATH)
 
             # generate a chart for feature importance
             log_feature_importance(train=X, model=regr)
@@ -121,19 +118,13 @@ if __name__ == "__main__":
 
     best_model_uri = f"{best_run.info.artifact_uri}/model"
 
-    # TODO: 베스트 모델을 아티팩트 폴더에 복사
-    copy_tree(
-              # TODO: 베스트 모델 URI에서 file:// 를 지울 것, 
-              ARTIFACT_PATH
-    )
+    copy_tree(best_model_uri.replace("file://", ""), ARTIFACT_PATH)
 
 
     # BentoML에 모델 저장
     bentoml.sklearn.save_model(
         name="house_rent",
-        model=mlflow.sklearn.load_model(
-            # TODO: 베스트 모델 URI
-        ),
+        model=mlflow.sklearn.load_model(best_model_uri),
         signatures={"predict": {"batchable": True, "batch_dim": 0}},
         metadata=best_params,
     )
